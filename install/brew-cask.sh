58d6089958e59b10d15a7f7d86e6ca4d5203a038
Kelsie Steele <kelsiesteele@gmail.com>
2015-04-10 16:26:42 +0300

    Customize dotfiles for personal use.

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -3,8 +3,6 @@
 apps=(
     1password
     alfred
-    applepi-baker
-    atom
     bartender
     carbon-copy-cloner
     cleanmymac
@@ -14,22 +12,14 @@ apps=(
     fantastical
     firefox
     google-chrome
-    google-drive
     hazel
-    hipchat
     istat-menus
     iterm2
-    little-snitch
-    mactracker
-    namebench
-    screenflick
     silverlight
     skype
     teamviewer
     things
     vagrant
-    virtualbox
-    vlc
 )
 
 brew cask install ${apps[@]} --appdir=/Applications
--- install/brew.sh
+++ install/brew.sh
@@ -2,12 +2,9 @@
 
 brew update
 
-brew_install_or_upgrade 'ansible'
 brew_install_or_upgrade 'bash-completion'
 brew_install_or_upgrade 'dockutil'
-brew_install_or_upgrade 'figlet'
 brew_install_or_upgrade 'htop-osx'
-brew_install_or_upgrade 'namebench'
 brew_install_or_upgrade 'python'
 brew_install_or_upgrade 'ssh-copy-id'
 brew_install_or_upgrade 'tree'
